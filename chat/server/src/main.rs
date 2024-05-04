016b39500b8425bce0688205a1515205bdf1ca78
Tanish Neema <tneema501@gmail.com>
2024-05-04 12:09:37 -0500

    working on adding in a database connection to this code

--- chat/server/src/main.rs
+++ chat/server/src/main.rs
@@ -1,17 +1,26 @@
-use std::io::{ErrorKind,Read, Write};
+use std::io::{ErrorKind, Read, Write};
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
 
+use rusqlite::Connection;
+
 const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
+const DATABASE_PATH: &str = "my_database.db";
 
 fn sleep() {
     thread::sleep(std::time::Duration::from_millis(100));
 }
+
 fn main() {
+    // Establish a connection to SQLite3 database
+    let conn = Connection::open(DATABASE_PATH).expect("Failed to open database connection");
+
     let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
-    server.set_nonblocking(true).expect("failed to initialize non blocking");
+    server
+        .set_nonblocking(true)
+        .expect("failed to initialize non blocking");
 
     let mut clients = vec![];
     let (tx, rx) = mpsc::channel::<String>();
@@ -23,17 +32,28 @@ fn main() {
             let tx = tx.clone();
             clients.push(socket.try_clone().expect("Failed to clone client"));
 
+            let conn = conn.close();
             thread::spawn(move || loop {
                 let mut buff = vec![0; MSG_SIZE];
 
                 match socket.read_exact(&mut buff) {
                     Ok(_) => {
-                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = buff
+                            .into_iter()
+                            .take_while(|&x| x != 0)
+                            .collect::<Vec<_>>();
                         let msg = String::from_utf8(msg).expect("Invalid utf8 message");
 
                         println!("{}: {:?}", addr, msg);
-                        tx.send(msg).expect("Failed to send msg to rx");                        
-                    },
+                        tx.send(msg).expect("Failed to send msg to rx");
+
+                        // Example: Insert message into the database
+                        conn.execute(
+                            "INSERT INTO messages (address, message) VALUES (?1, ?2)",
+                            &[&addr.to_string(), &msg],
+                        )
+                        .expect("Failed to insert message into database");
+                    }
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                     Err(_) => {
                         println!("Closing connection with: {}", addr);
@@ -44,12 +64,18 @@ fn main() {
             });
         }
         if let Ok(msg) = rx.try_recv() {
-            clients = clients.into_iter().filter_map(|mut client| {
-                let mut buff = msg.clone().into_bytes();
-                buff.resize(MSG_SIZE, 0);
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buff = msg.clone().into_bytes();
+                    buff.resize(MSG_SIZE, 0);
 
-                client.write_all(&buff).map(|_| client).ok()
-            }).collect::<Vec<_>>();
+                    client
+                        .write_all(&buff)
+                        .map(|_| client)
+                        .ok()
+                })
+                .collect::<Vec<_>>();
         }
         sleep();
     }
